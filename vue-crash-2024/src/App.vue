f9a60328eb8f51ea3eee63571c03ed7e4f98702c
libertus-libertus <libert.jobs@gmail.com>
2024-11-10 15:53:48 +0700

    To do list using Vue js

--- vue-crash-2024/src/App.vue
+++ vue-crash-2024/src/App.vue
@@ -1,40 +1,65 @@
-<script>
-  export default {
-    data() {
-      return {
-        name: "Libertus Sabebeget",
-        status: "pending",
-        tasks: ["Task one", "Task Two", "Task Three"],
-        link: 'https://google.com/',
-      }
-    },
+<script setup>
+  import { ref } from 'vue';
 
-    methods: {
-      toggleStatus() {
-        if (this.status === "Active") {
-          this.status = "Pending"
-        } else if (this.status === "Pending") {
-          this.status = "Inactive"
-        } else {
-          this.status = "Active"
-        }
-      }
+  const name = ref("Libertus Sabebeget");
+  const status = ref("Active");
+  const tasks = ref(["Task one", "Task Two", "Task Three"]);
+  const newTask = ref();
+
+  const toggleStatus = () => {
+    if (status.value === "Active") {
+      status.value = "Pending"
+    } else if (status.value === "Pending") {
+      status.value = "Inactive"
+    } else {
+      status.value = "Active"
     }
-  }
+  };
+
+  const addTask = () => {
+    if (newTask.value.trim() !== "") {
+      tasks.value.push(newTask.value)
+      newTask.value = ""
+    }
+  };
+
+  const deleteTask = (index) => {
+    tasks.value.splice(index, 1);
+  };
 </script>
 
 <template>
+  <h1>{{ name }}</h1>
   <h3>Pengkondisian:</h3>
   <p v-if="status === 'Active'">User is Active</p>
   <p v-else-if="status === 'Pending'">User is Pending</p>
   <p v-else>User is Inactive</p>
+  <button @click="toggleStatus">Change Status</button> <br><br>
 
-  <button v-on:click="toggleStatus">Change Status</button>
+  <form @submit.prevent="addTask" >
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" autofocus/>
+    <button type="submit">Submit</button>
+  </form>
 
-  <h3>Task (Perulangan):</h3>
-  <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
-  </ul>
-
-  <a :href="link">Click for Google</a>
+  <h3>Task:</h3>
+  <ol>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>
+        {{ task }}
+      </span>
+      <button class="btn" @click="deleteTask(index)"> Remove Task</button>
+    </li>
+  </ol>
 </template>
+
+<style scoped>
+  .btn {
+    background-color: red;
+    color: white;
+    border: none;
+  }
+  .btn:hover {
+    cursor: pointer;
+  }
+</style>
---
+++ vue-crash-2024/src/App2.vue
@@ -0,0 +1,21 @@
+<script>
+  export default {
+    
+  }
+</script>
+
+<template>
+  <h3>Pengkondisian:</h3>
+  <p v-if="status === 'Active'">User is Active</p>
+  <p v-else-if="status === 'Pending'">User is Pending</p>
+  <p v-else>User is Inactive</p>
+
+  <button @click="toggleStatus">Change Status</button>
+
+  <h3>Task (Perulangan):</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+
+  <a :href="link">Click for Google</a>
+</template>
