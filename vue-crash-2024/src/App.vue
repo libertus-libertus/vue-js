34a6a56de27672da5c0970472331ee84b0d2b0a1
libertus-libertus <libert.jobs@gmail.com>
2024-11-10 12:12:35 +0700

    Pengkondisian dan Perulangan

--- vue-crash-2024/src/App.vue
+++ vue-crash-2024/src/App.vue
@@ -1,47 +1,40 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+  export default {
+    data() {
+      return {
+        name: "Libertus Sabebeget",
+        status: "pending",
+        tasks: ["Task one", "Task Two", "Task Three"],
+        link: 'https://google.com/',
+      }
+    },
+
+    methods: {
+      toggleStatus() {
+        if (this.status === "Active") {
+          this.status = "Pending"
+        } else if (this.status === "Pending") {
+          this.status = "Inactive"
+        } else {
+          this.status = "Active"
+        }
+      }
+    }
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h3>Pengkondisian:</h3>
+  <p v-if="status === 'Active'">User is Active</p>
+  <p v-else-if="status === 'Pending'">User is Pending</p>
+  <p v-else>User is Inactive</p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <button v-on:click="toggleStatus">Change Status</button>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <h3>Task (Perulangan):</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+
+  <a :href="link">Click for Google</a>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
